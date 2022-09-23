51188eb147282e9853d6f342614498407c81995b
M. Cagatay Akkiran <109696768+cagatayakk@users.noreply.github.com>
2022-09-23 14:02:04 +0200

    Update MariaDB_on_EC2.sh

--- Database/MariaDB_on_EC2.sh
+++ Database/MariaDB_on_EC2.sh
@@ -10,8 +10,35 @@
 
 # Connect to EC2 instance with SSH.
 
-sudo yum update -y                     # Update yum package management and install MariaDB server.
-sudo yum install mariadb-server -y
+sudo yum update -y                     # Update yum package management .
+sudo yum install mariadb-server -y     # Install MariaDB server.
+sudo systemctl start mariadb           # Start MariaDB service.
+sudo systemctl status mariadb          # Check status of MariaDB service.
+sudo systemctl enable mariadb          # # Enable MariaDB service, so that MariaDB service will be activated on restarts.
+
+
+sudo mysql_secure_installation         # Setup secure installation of MariaDB.
+mysql -u root                          # Show that you can not log into mysql terminal without password anymore.
+mysql -u root -p                       # Connect to the MariaDB Server and open MySQL CLI with root user and password 
+USE mysql;
+SELECT Host, User, Password FROM user; # List the users defined in the server and show that it has now password and its encrypted.
+CREATE DATABASE demodb;                # Create new database named 'demodb'.
+SHOW DATABASES;                        # Show newly created database.
+CREATE USER demouser IDENTIFIED BY 'demo1234';     # Create a user
+GRANT ALL ON clarusdb.* TO clarususer IDENTIFIED BY 'clarus1234' WITH GRANT OPTION;    # Grant permissions to the user
+FLUSH PRIVILEGES;                      # Update privileges.
+EXIT;
+
+
+
+
+
+
+
+
+
+
+
 
 
 
